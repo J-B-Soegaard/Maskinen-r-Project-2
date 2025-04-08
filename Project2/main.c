85a51e19ac4615871a6739af4a15a198c5d8df11
jacobsoegaard <s245902@student.dtu.dk>
2025-04-08 11:44:05 +0200

    .

--- Project2/main.c
+++ Project2/main.c
@@ -2,5 +2,17 @@
 
 int main(void) {
     printf("c1\tc2\tc3\tc4\tc5\tc6\tc7\n");
+    printf("\t\t\t\t\t\t\t\t[]\tf1\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tf2\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tf3\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("\t\t\t\t\t\t\t\t[]\tf4\n");
+    printf("\t\t\t\t\t\t\t\t\n");
+    printf("last command\n");
+    printf("message:\n");
+    printf("input >");
+
     return 0;
 }
