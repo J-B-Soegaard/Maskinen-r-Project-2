52c61b78d0dbef90923a333f09be14362978b5a7
rqsmus <rasmuskochj@gmail.com>
2025-04-13 20:00:38 +0200

    Commit

--- Project2/commands.c
+++ Project2/commands.c
@@ -7,3 +7,23 @@ int LD(char fileName[])
         FILE *fp = fopen("unshuffled.txt", "r"); // hvis ikke den der er givet en rigtigt fil navn så tag den det deck der sorteret.
     }
 }
+
+int CHECK()
+{
+    int card_count = 0;
+    char card[3];                // 3, 2 for char + 1 for Null (ny linje)
+    char all_cards[52][3];       // Array med 52 kort og 3, 2 for char og 1 for ny linje some bruges til check
+
+    FILE *file_check = fopen("unshuffled.txt", "r");
+
+    while (fscanf(file_check, "%2s", card) == 1){
+      strcpy(all_cards[card_count], card); // Kopier alle kort ind til vores Tjek array
+      card_count++;
+    }
+
+    fclose(file_check);
+
+    //Arbejdsplads (Still in the making)
+
+  return 0; //No error
+}
