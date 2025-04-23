6dec5244c933812edfed52f8311f79121762886d
Thuy Pham <s246009@student.dtu.dk>
2025-04-23 14:56:10 +0200

    Update structures.h

--- Project2/structures.h
+++ Project2/structures.h
@@ -1,1 +1,13 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
 
+typedef struct {
+    char rank; 
+    char suit;  
+} Card;
+
+typedef struct Node {
+    Card card;
+    int visible;
+    struct Node* next;   
+} Node;
