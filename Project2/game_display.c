b4e77fb9c8d9ceba081a4c42d17311d339706b34
Thuy Pham <s246009@student.dtu.dk>
2025-04-23 14:38:39 +0200

    cards get displayed and hidden is  [ ]

--- Project2/game_display.c
+++ Project2/game_display.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void print_card(Card card, int visible) {
+    if (!visible) {
+        printf("[ ]");
+    } else {
+        printf("%c%c", card.rank, card.suit);
+    }
+}
