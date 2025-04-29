83e9e55f297f37e49c7e8d1ed8c5dc471a309a37
Thuy Pham <s246009@student.dtu.dk>
2025-04-29 15:41:20 +0200

    Create game_state.h

---
+++ Project2/game_state.h
@@ -0,0 +1,15 @@
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+#include "card.h"  
+
+typedef struct GameState {
+    CardNode *deck;           
+    Column *columns[7];       
+    Foundation *foundations[4];
+    char lastCommand[128];
+    char message[128];
+    int phase;                // 0 to start 1 to play
+} GameState;
+
+#endif
